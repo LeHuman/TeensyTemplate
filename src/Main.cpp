c174d44e5e1573acbc047334370ea8586d2a8123
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-05-24 23:49:04 -0500

    test code

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,4 +1,15 @@
+#include "Arduino.h"
 
 int main(void) {
+    pinMode(13, OUTPUT);
+    Serial.begin(115200);
+    Serial.println("Teensy On!");
+    delay(1000);
+    while (true) {
+        delay(500);
+        static bool on = true;
+        digitalWriteFast(13, (on = !on));
+        Serial.printf("Hello %d", millis());
+    }
     return 0;
 }
